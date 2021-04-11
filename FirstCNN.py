c53580bf8e6d34455567f81df24043b7cc042467
Madji <a.madji@esi-sba.dz>
2021-04-11 15:41:58 +0200

    -Labeling the Dataset with bounding boxes -Generating csv file of Labels -Using VGG16 model for transfer learning instead of three block VGG

--- FirstCNN.py
+++ FirstCNN.py
@@ -1,79 +1,71 @@
 import os
 import sys
+import pandas as pd
 from matplotlib import pyplot
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Dropout
+from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 
-# define cnn model
 def define_model():
-    model = Sequential()
-    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same',
-                     input_shape=(200, 200, 3)))
-    model.add(MaxPooling2D((2, 2)))
-    model.add(Dropout(0.2))
-    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-    model.add(MaxPooling2D((2, 2)))
-    model.add(Dropout(0.2))
-    model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-    model.add(MaxPooling2D((2, 2)))
-    model.add(Dropout(0.2))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
-    model.add(Dropout(0.2))
-    model.add(Dense(3, activation='softmax'))
-    # compile model
+    model = VGG16(include_top=False, input_shape=(200, 200, 3))
+    for layer in model.layers:
+        layer.trainable = False
+    flat1 = Flatten()(model.layers[-1].output)
+    class1 = Dense(128, activation='relu', kernel_initializer='he_uniform')(flat1)
+    output = Dense(3, activation='softmax')(class1)
+    model = Model(inputs=model.inputs, outputs=output)
     model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
 
-# plot diagnostic learning curves
 def summarize_diagnostics(history):
-    # plot loss
     pyplot.subplot(211)
     pyplot.title('Cross Entropy Loss')
     pyplot.plot(history.history['loss'], color='blue', label='train')
     pyplot.plot(history.history['val_loss'], color='orange', label='test')
-    # plot accuracy
     pyplot.subplot(212)
     pyplot.title('Classification Accuracy')
     pyplot.plot(history.history['accuracy'], color='blue', label='train')
     pyplot.plot(history.history['val_accuracy'], color='orange', label='test')
-    # save plot to file
     filename = sys.argv[0].split('/')[-1]
     pyplot.savefig(filename + '_plot.png')
     pyplot.close()
 
 
-# run the test harness for evaluating a model
 def run_test_harness():
-    # define model
+    traindf = pd.read_csv('Dataset/Train_labels.csv', dtype=str)
+    testdf = pd.read_csv('Dataset/Test_labels.csv', dtype=str)
     model = define_model()
-    # create data generator
     datagen = ImageDataGenerator(rescale=1.0 / 255.0)
-    # prepare iterators
-    train_it = datagen.flow_from_directory('Dataset/Images/Train/',
-                                           class_mode='categorical', batch_size=12, target_size=(200, 200))
-    test_it = datagen.flow_from_directory('Dataset/Images/Test/',
-                                          class_mode='categorical', batch_size=12, target_size=(200, 200))
-    # fit model
+
+    train_it = datagen.flow_from_dataframe(dataframe=traindf,
+                                           directory="Dataset/Train",
+                                           x_col="filename",
+                                           y_col="class",
+                                           subset="training",
+                                           batch_size=12,
+                                           class_mode="categorical",
+                                           target_size=(200, 200))
+    test_it = datagen.flow_from_dataframe(dataframe=testdf,
+                                          directory="Dataset/Test",
+                                          x_col="filename",
+                                          y_col="class",
+                                          subset="training",
+                                          batch_size=12,
+                                          class_mode="categorical",
+                                          target_size=(200, 200))
+
     history = model.fit(train_it, steps_per_epoch=len(train_it),
-                        validation_data=test_it, validation_steps=len(test_it), epochs=7, verbose=1)
-    # evaluate model
+                        validation_data=test_it, validation_steps=len(test_it), epochs=10, verbose=1)
     _, acc = model.evaluate(test_it, steps=len(test_it), verbose=1)
     print('> %.3f' % (acc * 100.0))
-    # learning curves
     summarize_diagnostics(history)
-    # saving the model
     model.save('model.h5')
 
 
-# entry point, run the test harness
 run_test_harness()
