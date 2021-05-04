609059367118c613db90e9c1dd7c8722e4d04d7d
Madji <a.madji@esi-sba.dz>
2021-05-04 10:17:18 +0200

    Fixing errors

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 DATASET = 'PASCAL_VOC'
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 NUM_WORKERS = 4
 BATCH_SIZE = 32
 IMAGE_SIZE = 416
 NUM_CLASSES = 20
-LEARNING_RATE = 1e-4
-WEIGHT_DECAY = 1e-4
+LEARNING_RATE = 3e-4
+WEIGHT_DECAY = 5e-4
 NUM_EPOCHS = 100
-CONF_THRESHOLD = 0.05
+CONF_THRESHOLD = 0.4
 MAP_IOU_THRESH = 0.5
 NMS_IOU_THRESH = 0.45
 GRID_SIZE = [IMAGE_SIZE // 32, IMAGE_SIZE // 16, IMAGE_SIZE // 8]
@@ -23,12 +23,11 @@ CHECKPOINT_FILE = "checkpoint.tar"
 IMG_DIR = DATASET + "/images/"
 LABEL_DIR = DATASET + "/labels/"
 
-ANCHORS = [ #Yolo anchors calculated using Kmeans
+ANCHORS = [  # Yolo anchors calculated using Kmeans
     [(0.28, 0.22), (0.38, 0.48), (0.9, 0.78)],
     [(0.07, 0.15), (0.15, 0.11), (0.14, 0.29)],
     [(0.02, 0.03), (0.04, 0.07), (0.08, 0.06)],
 ]
-
 
 scale = 1.1
 train_transforms = A.Compose(
@@ -56,10 +55,10 @@ train_transforms = A.Compose(
         A.Posterize(p=0.1),
         A.ToGray(p=0.1),
         A.ChannelShuffle(p=0.05),
-        A.Normalize(mean=[0, 0, 0], std=[1, 1, 1], max_pixel_value=255,),
+        A.Normalize(mean=[0, 0, 0], std=[1, 1, 1], max_pixel_value=255, ),
         ToTensorV2(),
     ],
-    bbox_params=A.BboxParams(format="yolo", min_visibility=0.4, label_fields=[],),
+    bbox_params=A.BboxParams(format="yolo", min_visibility=0.4, label_fields=[], ),
 )
 test_transforms = A.Compose(
     [
@@ -67,7 +66,7 @@ test_transforms = A.Compose(
         A.PadIfNeeded(
             min_height=IMAGE_SIZE, min_width=IMAGE_SIZE, border_mode=cv2.BORDER_CONSTANT
         ),
-        A.Normalize(mean=[0, 0, 0], std=[1, 1, 1], max_pixel_value=255,),
+        A.Normalize(mean=[0, 0, 0], std=[1, 1, 1], max_pixel_value=255, ),
         ToTensorV2(),
     ],
     bbox_params=A.BboxParams(format="yolo", min_visibility=0.4, label_fields=[]),
@@ -97,83 +96,83 @@ PASCAL_CLASSES = [
 ]
 
 COCO_LABELS = ['person',
- 'bicycle',
- 'car',
- 'motorcycle',
- 'airplane',
- 'bus',
- 'train',
- 'truck',
- 'boat',
- 'traffic light',
- 'fire hydrant',
- 'stop sign',
- 'parking meter',
- 'bench',
- 'bird',
- 'cat',
- 'dog',
- 'horse',
- 'sheep',
- 'cow',
- 'elephant',
- 'bear',
- 'zebra',
- 'giraffe',
- 'backpack',
- 'umbrella',
- 'handbag',
- 'tie',
- 'suitcase',
- 'frisbee',
- 'skis',
- 'snowboard',
- 'sports ball',
- 'kite',
- 'baseball bat',
- 'baseball glove',
- 'skateboard',
- 'surfboard',
- 'tennis racket',
- 'bottle',
- 'wine glass',
- 'cup',
- 'fork',
- 'knife',
- 'spoon',
- 'bowl',
- 'banana',
- 'apple',
- 'sandwich',
- 'orange',
- 'broccoli',
- 'carrot',
- 'hot dog',
- 'pizza',
- 'donut',
- 'cake',
- 'chair',
- 'couch',
- 'potted plant',
- 'bed',
- 'dining table',
- 'toilet',
- 'tv',
- 'laptop',
- 'mouse',
- 'remote',
- 'keyboard',
- 'cell phone',
- 'microwave',
- 'oven',
- 'toaster',
- 'sink',
- 'refrigerator',
- 'book',
- 'clock',
- 'vase',
- 'scissors',
- 'teddy bear',
- 'hair drier',
- 'toothbrush'
-]
+               'bicycle',
+               'car',
+               'motorcycle',
+               'airplane',
+               'bus',
+               'train',
+               'truck',
+               'boat',
+               'traffic light',
+               'fire hydrant',
+               'stop sign',
+               'parking meter',
+               'bench',
+               'bird',
+               'cat',
+               'dog',
+               'horse',
+               'sheep',
+               'cow',
+               'elephant',
+               'bear',
+               'zebra',
+               'giraffe',
+               'backpack',
+               'umbrella',
+               'handbag',
+               'tie',
+               'suitcase',
+               'frisbee',
+               'skis',
+               'snowboard',
+               'sports ball',
+               'kite',
+               'baseball bat',
+               'baseball glove',
+               'skateboard',
+               'surfboard',
+               'tennis racket',
+               'bottle',
+               'wine glass',
+               'cup',
+               'fork',
+               'knife',
+               'spoon',
+               'bowl',
+               'banana',
+               'apple',
+               'sandwich',
+               'orange',
+               'broccoli',
+               'carrot',
+               'hot dog',
+               'pizza',
+               'donut',
+               'cake',
+               'chair',
+               'couch',
+               'potted plant',
+               'bed',
+               'dining table',
+               'toilet',
+               'tv',
+               'laptop',
+               'mouse',
+               'remote',
+               'keyboard',
+               'cell phone',
+               'microwave',
+               'oven',
+               'toaster',
+               'sink',
+               'refrigerator',
+               'book',
+               'clock',
+               'vase',
+               'scissors',
+               'teddy bear',
+               'hair drier',
+               'toothbrush'
+               ]
--- loss.py
+++ loss.py
@@ -1,15 +1,15 @@
 import torch
 import torch.nn as nn
-from iou import intersection_over_union as iou
+from utils import intersection_over_union
 
 
-class Yololoss(nn.Model):
+class Yololoss(nn.Module):
     def __init__(self):
         super().__init__()
         self.mean_square_error = nn.MSELoss()
         self.BCE = nn.BCEWithLogitsLoss()
         self.cross_entropy = nn.CrossEntropyLoss()
-        self.sigmoid = nn.sigmoid
+        self.sigmoid = nn.Sigmoid()
         self.var_class = 1
         self.var_noObject = 10
         self.var_Obj = 1
@@ -20,22 +20,22 @@ class Yololoss(nn.Model):
         noObj = target[..., 0] == 0
 
         # No object loss
-        no_object_loss = self.BCE(predictions[..., 0][noObj], (target[..., 0:1][noObj]))
+        no_object_loss = self.BCE((predictions[..., 0:1][noObj]), (target[..., 0:1][noObj]),)
 
         # Object loss
         anchors = anchors.reshape(1, 3, 1, 1, 2)
         box_predictions = torch.cat([self.sigmoid(predictions[..., 1:3]), torch.exp(predictions[..., 3:5]) * anchors],
-                                    dim=1)
-        ious = iou(box_predictions[obj], target[..., 1:5][obj]).detach()
-        object_loss = self.BCE(predictions[..., 0:1][obj], (target[..., 0:1]))
+                                    dim=-1)
+        ious = intersection_over_union(box_predictions[obj], target[..., 1:5][obj]).detach()
+        object_loss = self.mean_square_error(self.sigmoid(predictions[..., 0:1][obj]), ious * target[..., 0:1][obj])
 
         # Box coordinates loss
         predictions[..., 1:3] = self.sigmoid(predictions[..., 1:3])
         target[..., 3:5] = torch.log(1e-16 + target[..., 3:5] / anchors)
-        box_loss = self.mse(predictions[..., 1:5][obj], target[..., 1:5][obj])
+        box_loss = self.mean_square_error(predictions[..., 1:5][obj], target[..., 1:5][obj])
 
         # Class loss
-        class_loss = self.entropy((predictions[..., 5][obj]), (target[..., 5][obj].long()))
+        class_loss = self.cross_entropy((predictions[..., 5:][obj]), (target[..., 5][obj].long()))
 
         return (self.var_box * box_loss
                 + self.var_Obj * object_loss
